a6ea969bd3f3c1318f0fc9f3461e3eb9f7cd58b3
Pacolias <pacomolinac2003@gmail.com>
2025-03-27 18:27:00 +0100

    Primer commit

---
+++ asset-management/src/test/java/com/miniinfomates2003/asset_management/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniinfomates2003.asset_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
