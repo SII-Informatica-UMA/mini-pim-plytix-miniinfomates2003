dd376c3fa7ceecc0c89033160ddc07d9049653f0
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2025-04-24 16:39:03 +0200

    DTOs

---
+++ asset-management/src/main/java/com/miniinfomates2003/asset_management/dtos/CategoriaDTO.java
@@ -0,0 +1,10 @@
+package com.miniinfomates2003.asset_management.dtos;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaDTO {
+    private Long id;
+    private String nombre;
+}
