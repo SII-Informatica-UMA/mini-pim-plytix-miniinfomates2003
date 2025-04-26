1f70db68f715c7946d3b143f213fdbbde4e6c6c9
Pacolias <pacomolinac2003@gmail.com>
2025-04-26 17:39:42 +0200

    Added/corrected: @RestController and RequestMapping("/activo") to ActivoController

--- asset-management/src/main/java/com/miniinfomates2003/asset_management/controllers/ActivoController.java
+++ asset-management/src/main/java/com/miniinfomates2003/asset_management/controllers/ActivoController.java
@@ -2,12 +2,22 @@ package com.miniinfomates2003.asset_management.controllers;
 
 import com.miniinfomates2003.asset_management.services.ActivoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/activo")
 public class ActivoController {
     private final ActivoService activoService;
 
     @Autowired
     public ActivoController(ActivoService activoService) {
         this.activoService = activoService;
+    }
+
+    @PutMapping
+    public void updateActivo() {
+
     }
 }
